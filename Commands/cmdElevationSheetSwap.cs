e5bca97daf08fcab6cf17a41561f180246494141
Tom E Flaherty <teflaherty@utexas.edu>
2024-08-08 21:38:01 -0500

    3 errors

---
+++ Commands/cmdElevationSheetSwap.cs
@@ -0,0 +1,141 @@
+﻿using LifestyleDesign_r24.Common;
+
+namespace LifestyleDesign_r24
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdElevationSheetSwap : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // this is a variable for the Revit application
+            UIApplication uiapp = commandData.Application;
+
+            // this is a variable for the current Revit model
+            Document doc = uiapp.ActiveUIDocument.Document;
+
+            // get all viewports
+            FilteredElementCollector vpCollector = new FilteredElementCollector(doc);
+            vpCollector.OfCategory(BuiltInCategory.OST_Viewports);
+
+            // filter viewports for sheet names that contains "Exterior Elevations"
+            List<Viewport> eSheets = new List<Viewport>();
+            List<ViewSheet> lrSheets = new List<ViewSheet>();
+            List<string> sheetNum = new List<string>();
+
+            foreach (Viewport vPort in vpCollector)
+            {
+                ViewSheet curSheet = doc.GetElement(vPort.SheetId) as ViewSheet;
+                View curView = doc.GetElement(vPort.ViewId) as View;
+
+                string sName = Utils.GetParameterValueByName(curSheet, "Sheet Name");
+                string vName = curView.Name;
+
+                if (sName.Contains("Exterior Elevations"))
+                {
+                    if (vName.Contains("Left") || vName.Contains("Right"))
+                        lrSheets.Add(curSheet);
+                }
+            }
+
+            frmElevationSheetSwap curForm = new frmElevationSheetSwap(lrSheets)
+            {
+                Width = 375,
+                Height = 275,
+                WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen,
+                Topmost = true,
+            };
+
+            curForm.ShowDialog();
+
+            if (curForm.DialogResult == true)
+            {
+
+                // create variable for current sheet numbers
+                string curLeftNum1 = Utils.CleanSheetNumber(curForm.GetComboBoxLeft1Item());
+                string curRightNum1 = Utils.CleanSheetNumber(curForm.GetComboBoxRight1Item());
+
+                string numLeft1 = Utils.GetStringBetweenCharacters(curLeftNum1, curLeftNum1[0].ToString(),
+                    curLeftNum1[curLeftNum1.Length - 1].ToString());
+
+                string numRight1 = Utils.GetStringBetweenCharacters(curRightNum1, curRightNum1[0].ToString(),
+                    curRightNum1[curRightNum1.Length - 1].ToString());
+
+                if (curForm.GetCheckBox1() == true)
+                {
+                    string curLeftNum2 = curForm.GetComboBoxLeft2Item();
+                    string curRightNum2 = curForm.GetComboBoxRight2Item();
+
+                    string numLeft2 = Utils.GetStringBetweenCharacters(curLeftNum2, curLeftNum2[0].ToString(),
+                   curLeftNum2[curLeftNum2.Length - 1].ToString());
+
+                    string numRight2 = Utils.GetStringBetweenCharacters(curRightNum2, curRightNum2[0].ToString(),
+                        curRightNum2[curRightNum2.Length - 1].ToString());
+                }
+
+                // hide project browser
+                DockablePaneId dpId = DockablePanes.BuiltInDockablePanes.ProjectBrowser;
+                DockablePane pB = new DockablePane(dpId);
+                pB.Hide();
+
+                // start the transaction
+                using (Transaction t = new Transaction(doc))
+                {
+                    t.Start("Reorder Elevation Sheets");
+
+                    List<ViewSheet> matchingLeftSheets = Utils.GetSheetsByNumber(doc, numLeft1);
+                    List<ViewSheet> matchingRightSheets = Utils.GetSheetsByNumber(doc, numRight1);
+
+                    foreach (ViewSheet sheet in matchingLeftSheets)
+                    {
+                        sheet.SheetNumber = sheet.SheetNumber.Replace(numLeft1, numRight1 + "zz");
+                    }
+
+                    foreach (ViewSheet sheet in matchingRightSheets)
+                    {
+                        sheet.SheetNumber = sheet.SheetNumber.Replace(numRight1, numLeft1);
+                    }
+
+                    foreach (ViewSheet sheet in matchingLeftSheets)
+                    {
+                        sheet.SheetNumber = sheet.SheetNumber.Replace(numRight1 + "zz", numRight1);
+                    }
+
+                    t.Commit();
+                    pB.Show();
+
+                    string msgText = "Elevation sheets have been reordered.";
+                    string msgTitle = "Complete";
+                    Forms.MessageBoxButton msgButtons = Forms.MessageBoxButton.OK;
+
+                    Forms.MessageBox.Show(msgText, msgTitle, msgButtons, Forms.MessageBoxImage.Information);
+                }
+            }
+
+            return Result.Succeeded;
+        }
+        internal static PushButtonData GetButtonData()
+        {
+            // use this method to define the properties for this command in the Revit ribbon
+            string buttonInternalName = "btnCommand1";
+            string buttonTitle = "Button 1";
+            string? methodBase = MethodBase.GetCurrentMethod().DeclaringType?.FullName;
+
+            if (methodBase == null)
+            {
+                throw new InvalidOperationException("MethodBase.GetCurrentMethod().DeclaringType?.FullName is null");
+            }
+            else
+            {
+                Common.ButtonDataClass myButtonData1 = new Common.ButtonDataClass(
+                    buttonInternalName,
+                    buttonTitle,
+                    methodBase,
+                    Properties.Resources.Blue_32,
+                    Properties.Resources.Blue_16,
+                    "This is a tooltip for Button 1");
+
+                return myButtonData1.Data;
+            }
+        }
+    }
+}
---
+++ WPF_Forms/frmElevationSheetSwap.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LifestyleDesign_r24
+{
+    /// <summary>
+    /// Interaction logic for frmElevationSheetSwap.xaml
+    /// </summary>
+    public partial class frmElevationSheetSwap : Window
+    {
+        public frmElevationSheetSwap(List<ViewSheet> elevSheets)
+        {
+            InitializeComponent();
+
+            tbkLeft2.IsEnabled = false;
+            tbkRight2.IsEnabled = false;
+            cbxLeft2.IsEnabled = false;
+            cbxRight2.IsEnabled = false;
+
+            chbSplit.IsChecked = false;
+
+            foreach (ViewSheet sheet in elevSheets)
+            {
+                cbxLeft1.Items.Add(sheet.SheetNumber);
+                cbxRight1.Items.Add(sheet.SheetNumber);
+                cbxLeft2.Items.Add(sheet.SheetNumber);
+                cbxRight2.Items.Add(sheet.SheetNumber);
+            }
+        }
+
+        private void chbSplit_Checked(object sender, RoutedEventArgs e)
+        {
+            // if checked show bottom combo boxes
+
+            // not then hide bottom combo boxes
+
+            CheckBox cBox = (CheckBox)sender;
+
+            if (cBox.IsChecked == true) { tbkLeft2.IsEnabled = true; cbxLeft2.IsEnabled = true; tbkRight2.IsEnabled = true; cbxRight2.IsEnabled = true; }
+            else { cbxLeft2.IsEnabled = false; cbxRight2.IsEnabled = false; tbkLeft2.IsEnabled = false; tbkRight2.IsEnabled = false; }
+        }
+
+        internal bool GetCheckBox1()
+        {
+            if (chbSplit.IsChecked == true)
+                return true;
+
+            return false;
+        }
+
+        internal string GetComboBoxLeft1Item()
+        {
+            return cbxLeft1.SelectedItem.ToString();
+        }
+
+        internal string GetComboBoxRight1Item()
+        {
+            return cbxRight1.SelectedItem.ToString();
+        }
+
+        internal string GetComboBoxLeft2Item()
+        {
+            return cbxLeft2.SelectedItem.ToString();
+        }
+
+        internal string GetComboBoxRight2Item()
+        {
+            return cbxRight2.SelectedItem.ToString();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
