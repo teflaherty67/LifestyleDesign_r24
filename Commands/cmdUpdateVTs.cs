f438c333c5569bb38f854de1e1a47803e33a86b8
Tom E Flaherty <teflaherty@utexas.edu>
2024-08-29 23:27:16 -0500

    transaction group failed

--- Commands/cmdUpdateVTs.cs
+++ Commands/cmdUpdateVTs.cs
@@ -26,135 +26,169 @@ namespace LifestyleDesign_r24
             // set the path to the view template file
             string templateDoc = "S:\\Shared Folders\\Lifestyle USA Design\\Library 2025\\Template\\View Templates.rvt";
 
-            // create a variable for the source document & open it
+            // create a variable for the source document
             Document sourceDoc = uidoc.Application.OpenAndActivateDocument(templateDoc).Document;
 
-            // create & start the transaction
-            using (Transaction t = new Transaction(curDoc, "Update View Templates"))
+            // create a transaction group
+            using (TransactionGroup tGroup = new TransactionGroup(curDoc, "Update View Templates"))
             {
-                t.Start();
-
-                // delete all view templates that start with a letter or a number
-                foreach (View curVT in curVTs)
+                // create the 1st transaction
+                using (Transaction t = new Transaction(curDoc))
                 {
-                    // get the name of the view template
-                    string curName = curVT.Name;
+                    // start the transaction group
+                    tGroup.Start();
 
-                    // check if first character is letter
-                    bool isLetter = !String.IsNullOrEmpty(curName) && Char.IsLetter(curName[0]);
+                    #region Delete View Templates
 
-                    // check if first two charactera is number                    
-                    bool isNumber = !String.IsNullOrEmpty(curName) && Char.IsNumber(curName[0]);
+                    // start the 1st transaction
+                    t.Start("Delete View Templates");
 
-                    // if yes, delete it
-                    if (isLetter == true || isNumber == true)
+                    // delete all view templates that start with a letter or a number
+                    foreach (View curVT in curVTs)
                     {
-                        try
+                        // get the name of the view template
+                        string curName = curVT.Name;
+
+                        // check if first character is letter
+                        bool isLetter = !String.IsNullOrEmpty(curName) && Char.IsLetter(curName[0]);
+
+                        // check if first two charactera is number                    
+                        bool isNumber = !String.IsNullOrEmpty(curName) && Char.IsNumber(curName[0]);
+
+                        // if yes, delete it
+                        if (isLetter == true || isNumber == true)
                         {
-                            curDoc.Delete(curVT.Id);
+                            try
+                            {
+                                curDoc.Delete(curVT.Id);
+                            }
+                            catch (Exception)
+                            {
+                            }
                         }
-                        catch (Exception)
+                    }
+
+                    // commit the 1st transaction
+                    t.Commit();
+
+                    #endregion
+
+                    // create a variable for the target document                  
+                    Document targetDoc = uidoc.Document;
+
+                    // get the view templates from the source document
+                    List<View> listViewTemplates = new FilteredElementCollector(sourceDoc)
+                        .OfClass(typeof(View))
+                        .Cast<View>()
+                        .Where(v => v.IsTemplate)
+                        .ToList();
+
+                    #region Transfer View Templates
+
+                    // start the 2nd transaction
+                    t.Start("Transfer View Teamplates");
+
+                    // transfer the vew templates from the source document
+                    foreach (View sourceTemplate in listViewTemplates)
+                    {
+                        ElementId newTemplateID = Utils.ImportViewTemplates(sourceDoc, sourceTemplate, targetDoc);
+                    }
+
+                    t.Commit();
+
+                    #endregion
+
+                    // create a variable for the new view template
+                    View newViewTemp = null;
+
+                    #region Assign View Templates
+
+                    // start the 3rd transaction
+                    t.Start("Assign View Teamplates");
+
+                    foreach (View curView in nonTemplateViews)
+                    {
+                        // assign the appropriate view template
+                        if (curView.Name.Contains("Annotation", StringComparison.Ordinal))
                         {
+                            newViewTemp = Utils.GetViewTemplateByNameContains(curDoc, "Annotations");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
+                        }
+                        else if (curView.Name.Contains("Dimensions", StringComparison.Ordinal))
+                        {
+                            newViewTemp = Utils.GetViewTemplateByNameContains(curDoc, "Dimensions");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
+                        }
+                        else if (curView.Category.Equals("02:Exterior Elevations"))
+                        {
+                            newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "02:Exterior Elevations");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
+                        }
+                        else if (curView.Name.Contains("Roof", StringComparison.Ordinal))
+                        {
+                            newViewTemp = Utils.GetViewTemplateByNameContains(curDoc, "Roof");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
+                        }
+                        else if (curView.Category.Equals("04:Sections"))
+                        {
+                            newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "04:Sections");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
+                        }
+                        else if (curView.Category.Equals("05:Interior Elevations"))
+                        {
+                            newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "05:Interior Elevations");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
+                        }
+                        else if (curView.Name.Contains("Electrical", StringComparison.Ordinal))
+                        {
+                            newViewTemp = Utils.GetViewTemplateByNameContains(curDoc, "Electrical");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
+                        }
+                        else if (curView.Name.Contains("Form", StringComparison.Ordinal))
+                        {
+                            newViewTemp = Utils.GetViewTemplateByNameContains(curDoc, "Form");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
+                        }
+                        else if (curView.Category.Equals("10:Floor Areas"))
+                        {
+                            newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "10:Floor Areas");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
+                        }
+                        else if (curView.Category.Equals("11:Frame Areas"))
+                        {
+                            newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "11:Frame Areas");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
+                        }
+                        else if (curView.Category.Equals("12:Attic Areas"))
+                        {
+                            newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "12:Attic Areas");
+
+                            curView.ViewTemplateId = newViewTemp.Id;
                         }
                     }
+
+                    // commit the 3rd transaction
+                    t.Commit();
+
+                    #endregion
+
+                    tGroup.Assimilate();
                 }
-
-                // transfer view templates from template file                
-                Document targetDoc = uidoc.Document;
-
-                // get the view templates from the source document
-                List<View> listViewTemplates = new FilteredElementCollector(sourceDoc)
-                    .OfClass(typeof(View))
-                    .Cast<View>()
-                    .Where(v => v.IsTemplate)
-                    .ToList();
-
-                // transfer the vew templates from the source document
-                foreach (View sourceTemplate in listViewTemplates)
-                {                    
-                    ElementId newTemplateID = Utils.ImportViewTemplates(sourceDoc, sourceTemplate, targetDoc);
-                }
-
-                // create a variable for the new view template
-                View newViewTemp = null;
-
-                // loop through the non-template views
-                foreach (View curView in nonTemplateViews)
-                {
-                    // assign the appropriate view template
-                    if (curView.Name.Contains("Annotation", StringComparison.Ordinal))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByNameContains(curDoc, "Annotations");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                    else if (curView.Name.Contains("Dimensions", StringComparison.Ordinal))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByNameContains(curDoc, "Dimensions");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                    else if (curView.Category.Equals("02:Exterior Elevations"))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "02:Exterior Elevations");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                    else if (curView.Name.Contains("Roof", StringComparison.Ordinal))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByNameContains(curDoc, "Roof");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                    else if (curView.Category.Equals("04:Sections"))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "04:Sections");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                    else if (curView.Category.Equals("05:Interior Elevations"))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "05:Interior Elevations");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                    else if (curView.Name.Contains("Electrical", StringComparison.Ordinal))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByNameContains(curDoc, "Electrical");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                    else if (curView.Name.Contains("Form", StringComparison.Ordinal))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByNameContains(curDoc, "Form");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                    else if (curView.Category.Equals("10:Floor Areas"))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "10:Floor Areas");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                    else if (curView.Category.Equals("11:Frame Areas"))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "11:Frame Areas");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                    else if (curView.Category.Equals("12:Attic Areas"))
-                    {
-                        newViewTemp = Utils.GetViewTemplateByCategoryEquals(curDoc, "12:Attic Areas");
-
-                        curView.ViewTemplateId = newViewTemp.Id;
-                    }
-                }                
-
-                t.Commit();
             }
             
             return Result.Succeeded;
         }
+
         internal static PushButtonData GetButtonData()
         {
             // use this method to define the properties for this command in the Revit ribbon
